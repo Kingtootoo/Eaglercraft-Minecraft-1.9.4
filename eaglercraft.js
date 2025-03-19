2d9da26be5d9809a0f999ca6ebb14dacba7e571d
Felix <159484470+Kingtootoo@users.noreply.github.com>
2025-03-19 23:39:27 +0300

    Update eaglercraft.js

--- eaglercraft.js
+++ eaglercraft.js
@@ -8,3 +8,23 @@ function loadShieldTextures() {
   var shieldTexture = new Image();
   shieldTexture.src = 'assets/textures/shield.png';
 }
+var shieldTexture, shieldBlockTexture;
+
+function loadShieldTextures() {
+  // Load the shield texture for holding in the inventory
+  shieldTexture = new Image();
+  shieldTexture.src = 'assets/textures/shield.png'; // Path to the shield texture
+
+  // Load the shield texture for blocking
+  shieldBlockTexture = new Image();
+  shieldBlockTexture.src = 'assets/textures/shield_block.png'; // Path to the shield blocking texture
+
+  // Log to confirm textures are loaded
+  shieldTexture.onload = function() {
+    console.log("Shield texture loaded");
+  };
+
+  shieldBlockTexture.onload = function() {
+    console.log("Shield blocking texture loaded");
+  };
+}
